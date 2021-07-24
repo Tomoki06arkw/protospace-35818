bbfbeda22182b3c45e83d00c810bdc2a3a3df4c7
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-07-24 15:13:36 +0900

    修正

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Protospace35818
+module Protophoto0616
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
