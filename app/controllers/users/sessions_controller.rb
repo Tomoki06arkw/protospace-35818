c5830986c268c655728d520b045a9e2c2b6be2c1
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-08-07 10:32:31 +0900

    ゲストユーザー機能実装

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,11 @@ class Users::SessionsController < Devise::SessionsController
   def new_guest
     user = User.find_or_create_by(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
+      
+      user.name = 'ゲスト'
+      user.profile = '宜しく'
+      user.occupation = 'テック'
+      user.position = '社長'
     end
     sign_in user
     redirect_to root_path, notice: 'ゲストユーザーログイン'
