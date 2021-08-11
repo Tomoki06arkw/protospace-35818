d056d939586fab6fb9e48174864aaef6d047762d
Tomoki06arkw <tomoki.tomto@gmail.com>
2021-08-11 16:05:38 +0900

    push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,9 +66,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'http:///prototype-35818.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http:///prototype-35818.herokuapp.com/' }
   config.action_mailer.raise_delivery_errors = false
-
+  
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
     :address => "smtp.gmail.com",
